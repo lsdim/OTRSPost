d822a25212403757999ff857263459a28d6d3903
lsdim <webmasi@mail.ru>
2024-11-15 16:14:16 +0200

    add get chat and bot id

--- bot.py
+++ bot.py
@@ -11,6 +11,12 @@ def handle_message(update, context):
         context.bot.send_message(chat_id=chat_id, text="Я тут!")
     elif "заявк" in text.lower():
         context.bot.send_message(chat_id=chat_id, text="Всі заявки в мене на контролі!")
+    elif "чат ід" in text.lower():
+        # Відправка chat_id у відповідь
+        context.bot.send_message(chat_id=chat_id, text=f"Ідентифікатор цього чату: {chat_id}")
+    elif "твій ід" in text.lower():
+        bot_id = context.bot.id
+        context.bot.send_message(chat_id=chat_id, text=f"Мій ідентифікатор: {bot_id}")
 
 def main():
     updater = Updater("7255647619:AAH0dKnIaCsFRx7Dg2qyezOWuum4ItZBkec", use_context=True)
