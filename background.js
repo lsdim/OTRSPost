d79d0fb339bd0c621c55d142902a65a2fb7c9d36
lsdim <webmasi@mail.ru>
2025-08-07 10:14:08 +0300

    add self Uninstall

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
+browser.runtime.onInstalled.addListener(async (details) => {
+    const { previousVersion, reason } = details;
+    const { version } = browser.runtime.getManifest();
 
-browser.runtime.onInstalled.addListener(function (details) {
-    if (details.reason === "install") {
-        browser.tabs.create({
-            url: "popup/policy.html"
-        });
+    if (reason === 'install') {
+        browser.tabs.create({ url: 'popup/policy.html' });
+    } else if (reason === 'update') {
+        const { agreed, version: storedVersion } = await browser.storage.local.get(['accept-consent', 'version']);
+        if (!agreed || storedVersion !== version) {
+            browser.tabs.create({ url: 'popup/policy.html' });
+        }
     }
 });
--- popup/policy.js
+++ popup/policy.js
@@ -1,11 +1,8 @@
-document.getElementById('accept-consent').addEventListener('click', function () {
-    browser.storage.local.set({ consentGiven: true }).then(() => {
-        window.close();
-    });
+document.getElementById('accept-consent').addEventListener('click', () => {
+    browser.storage.local.set({ consentGiven: true, version: browser.runtime.getManifest().version });
+    window.close();
 });
 
-document.getElementById('reject-consent').addEventListener('click', function () {
-    browser.storage.local.set({ consentGiven: false }).then(() => {
-        window.close();
-    });
+document.getElementById('reject-consent').addEventListener('click', () => {
+    browser.management.uninstallSelf();
 });
